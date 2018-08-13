06f8f8fdbea3c5f2fdf24dc13b4ed7255ccbb089
Unknown <fireflygirl11@gmail.com>
2018-08-13 16:11:01 -0600

    Database stuff

--- ExplodingKittens/sysdiagram.cs
+++ ExplodingKittens/sysdiagram.cs
@@ -12,12 +12,12 @@ namespace ExplodingKittens
     using System;
     using System.Collections.Generic;
     
-    public partial class Game
+    public partial class sysdiagram
     {
-        public int GameID { get; set; }
-        public Nullable<int> CurrentPlayer { get; set; }
-        public string Name { get; set; }
-    
-        public virtual Player Player { get; set; }
+        public string name { get; set; }
+        public int principal_id { get; set; }
+        public int diagram_id { get; set; }
+        public Nullable<int> version { get; set; }
+        public byte[] definition { get; set; }
     }
 }
