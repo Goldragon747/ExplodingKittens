1021cce6d59f51d1462bc0a0160bda97a7f5e0f6
Unknown <SKnowlton@student.neumont.edu>
2018-08-23 00:00:39 -0600

    Hard-coded cards

--- ExplodingKittensLib/Models/WpfDeck.cs
+++ ExplodingKittensLib/Models/WpfDeck.cs
@@ -1,37 +1,203 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 using ExplodingKittensLib.Models.Cards;
 
 namespace ExplodingKittensLib.Models
 {
-    public class WpfDeck : Deck
+    public class WPFDeck : Deck
     {
-        public WpfDeck(Game game, int numberOfPlayers)
+        int id = 0;
+        public WPFDeck(Game game, int numberOfPlayers)
             : base(game, numberOfPlayers)
         {
+            AddIntialCards();
         }
 
+        public Stack<Card> GetNopeCards()
+        {
+            Stack<Card> cards = new Stack<Card>();
+            //todo M: Set Nope card images
+            Nope one = new Nope(Game, id++);
+            one.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            Nope two = new Nope(Game, id++);
+            two.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            Nope three = new Nope(Game, id++);
+            three.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            Nope four = new Nope(Game, id++);
+            four.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            Nope five = new Nope(Game, id++);
+            five.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            cards.Push(one);
+            cards.Push(two);
+            cards.Push(three);
+            cards.Push(four);
+            cards.Push(five);
+
+            return cards;
+        }
+        public Stack<Card> GetAttackCards()
+        {
+            Stack<Card> cards = new Stack<Card>();
+            //todo M: Set Attack card images
+            Attack one = new Attack(Game, id++);
+            one.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            Attack two = new Attack(Game, id++);
+            two.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            Attack three = new Attack(Game, id++);
+            three.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            Attack four = new Attack(Game, id++);
+            four.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            cards.Push(one);
+            cards.Push(two);
+            cards.Push(three);
+            cards.Push(four);
+
+            return cards;
+        }
+        public Stack<Card> GetSkipCards()
+        {
+            Stack<Card> cards = new Stack<Card>();
+            //todo M: Set Skip card images
+            Skip one = new Skip(Game, id++);
+            one.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            Skip two = new Skip(Game, id++);
+            two.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            Skip three = new Skip(Game, id++);
+            three.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            Skip four = new Skip(Game, id++);
+            four.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            cards.Push(one);
+            cards.Push(two);
+            cards.Push(three);
+            cards.Push(four);
+
+            return cards;
+        }
+        public Stack<Card> GetFavorCards()
+        {
+            Stack<Card> cards = new Stack<Card>();
+            //todo M: Set Favor card images
+            Favor one = new Favor(Game, id++);
+            one.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            Favor two = new Favor(Game, id++);
+            two.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            Favor three = new Favor(Game, id++);
+            three.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            Favor four = new Favor(Game, id++);
+            four.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            cards.Push(one);
+            cards.Push(two);
+            cards.Push(three);
+            cards.Push(four);
+
+            return cards;
+        }
+        public Stack<Card> GetShuffleCards()
+        {
+            Stack<Card> cards = new Stack<Card>();
+            //todo M: Set Shuffle card images
+            Shuffle one = new Shuffle(Game, id++);
+            one.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            Shuffle two = new Shuffle(Game, id++);
+            two.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            Shuffle three = new Shuffle(Game, id++);
+            three.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            Shuffle four = new Shuffle(Game, id++);
+            four.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            cards.Push(one);
+            cards.Push(two);
+            cards.Push(three);
+            cards.Push(four);
+
+            return cards;
+        }
+        public Stack<Card> GetSeeTheFutureCards()
+        {
+            Stack<Card> cards = new Stack<Card>();
+            //todo M: Set SeeTheFuture card images
+            SeeTheFuture one = new SeeTheFuture(Game, id++);
+            one.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            SeeTheFuture two = new SeeTheFuture(Game, id++);
+            two.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            SeeTheFuture three = new SeeTheFuture(Game, id++);
+            three.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            SeeTheFuture four = new SeeTheFuture(Game, id++);
+            four.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            SeeTheFuture five = new SeeTheFuture(Game, id++);
+            five.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            cards.Push(one);
+            cards.Push(two);
+            cards.Push(three);
+            cards.Push(four);
+            cards.Push(five);
+
+            return cards;
+        }
+        public Stack<Card> GetCatCards()
+        {
+            Stack<Card> cards = new Stack<Card>();
+            //todo M: Set Cat card images
+            //Tacocat 4
+            for (int i = 0; i < 4; i++)
+            {
+                Tacocat card = new Tacocat(Game, id++);
+                card.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+                cards.Push(card);
+            }
+            //Rainbow - Ralphing Cat 4
+            for (int i = 0; i < 4; i++)
+            {
+                RainbowRalphingCat card = new RainbowRalphingCat(Game, id++);
+                card.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+                cards.Push(card);
+            }
+            //Beard Cat 4
+            for (int i = 0; i < 4; i++)
+            {
+                BeardedCat card = new BeardedCat(Game, id++);
+                card.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+                cards.Push(card);
+            }
+            //Hairy Potato Cat 4
+            for (int i = 0; i < 4; i++)
+            {
+                HairyPotatoCat card = new HairyPotatoCat(Game, id++);
+                card.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+                cards.Push(card);
+            }
+            //Cattermelon 4
+            for (int i = 0; i < 4; i++)
+            {
+                Cattermelon card = new Cattermelon(Game, id++);
+                card.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+                cards.Push(card);
+            }
+
+            return cards;
+        }
         public override Stack<Card> GetDefuseCards()
         {
             Stack<Card> cards = new Stack<Card>();
             if (NumberOfPlayers != 2)//todo Figure out where the randomization should be done and do it
             {
                 //todo M: Set Defuse card images
-                Defuse one = new Defuse(Game, 0);
-                one.CardImage = System.Drawing.Image.FromFile("Assets/demo-card.jpg");
-                Defuse two = new Defuse(Game, 1);
-                two.CardImage = System.Drawing.Image.FromFile("Assets/demo-card.jpg");
-                Defuse three = new Defuse(Game, 2);
-                three.CardImage = System.Drawing.Image.FromFile("Assets/demo-card.jpg");
-                Defuse four = new Defuse(Game, 3);
-                four.CardImage = System.Drawing.Image.FromFile("Assets/demo-card.jpg");
-                Defuse five = new Defuse(Game, 4);
-                five.CardImage = System.Drawing.Image.FromFile("Assets/demo-card.jpg");
-                Defuse six = new Defuse(Game, 5);
-                six.CardImage = System.Drawing.Image.FromFile("Assets/demo-card.jpg");
+                Defuse one = new Defuse(Game, id++);
+                one.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+                Defuse two = new Defuse(Game, id++);
+                two.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+                Defuse three = new Defuse(Game, id++);
+                three.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+                Defuse four = new Defuse(Game, id++);
+                four.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+                Defuse five = new Defuse(Game, id++);
+                five.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+                Defuse six = new Defuse(Game, id++);
+                six.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
                 cards.Push(one);
                 cards.Push(two);
                 cards.Push(three);
@@ -41,50 +207,47 @@ namespace ExplodingKittensLib.Models
             }
             return cards;
         }
-
-        public Stack<Card> GetExplodingKittenCards()
+        public Stack<Card> GetExplodingKittenCards(int id)
         {
             Stack<Card> cards = new Stack<Card>();
-            if (NumberOfPlayers != 2)//todo Figure out where the randomization should be done and do it
-            {
-                //todo M: Set ExplodingKitten card images
-                ExplodingKitten one = new ExplodingKitten(Game, 6);
-                one.CardImage = System.Drawing.Image.FromFile("Assets/demo-card.jpg");
-                ExplodingKitten two = new ExplodingKitten(Game, 7);
-                two.CardImage = System.Drawing.Image.FromFile("Assets/demo-card.jpg");
-                ExplodingKitten three = new ExplodingKitten(Game, 8);
-                three.CardImage = System.Drawing.Image.FromFile("Assets/demo-card.jpg");
-                ExplodingKitten four = new ExplodingKitten(Game, 9);
-                four.CardImage = System.Drawing.Image.FromFile("Assets/demo-card.jpg");
-                cards.Push(one);
-                cards.Push(two);
-                cards.Push(three);
-                cards.Push(four);
-            }
+            //todo M: Set ExplodingKitten card images
+            ExplodingKitten one = new ExplodingKitten(Game, id++);
+            one.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            ExplodingKitten two = new ExplodingKitten(Game, id++);
+            two.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            ExplodingKitten three = new ExplodingKitten(Game, id++);
+            three.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            ExplodingKitten four = new ExplodingKitten(Game, id++);
+            four.CardImage = new BitmapImage(new Uri("pack://application:,,,/ExplodingKittensLib;Component/Images/demo-card.jpg", UriKind.Absolute));
+            cards.Push(one);
+            cards.Push(two);
+            cards.Push(three);
+            cards.Push(four);
             return cards;
         }
 
-        private Stack<Card> MakeCards()
+        private void AddIntialCards()
         {
-            bool done = false;
-            while (!done)
+            AddToDrawPile(GetNopeCards());//Nope 5
+            AddToDrawPile(GetAttackCards());//Attack 4
+            AddToDrawPile(GetSkipCards());//Skip 4
+            AddToDrawPile(GetFavorCards());//Favor 4
+            AddToDrawPile(GetShuffleCards());//Shuffle 4
+            AddToDrawPile(GetSeeTheFutureCards());//See the future 5
+            AddToDrawPile(GetCatCards());
+        }
+
+        public override void AddExplodingKittenCards()
+        {
+            AddToDrawPile(GetExplodingKittenCards(DrawPile.Count));//Exploding Kitten 4
+        }
+
+        private void AddToDrawPile(Stack<Card> cards)
+        {
+            foreach (Card card in cards)
             {
-                GetExplodingKittenCards();//Exploding Kitten 4
-                GetDefuseCards();//Defuse 6
-                //Nope 5
-                //Attack 4
-                //Skip 4
-                //Favor 4
-                //Shuffle 4
-                //See the future 5
-                //Tacocat 4
-                //Rainbow - Ralphing Cat 4
-                //Beard Cat 4
-                //Hairy Potato Cat 4
-                //Cattermelon 4
+                DrawPile.Push(card);
             }
-            //todo Get cards from database
-            throw new NotImplementedException();
         }
     }
 }
